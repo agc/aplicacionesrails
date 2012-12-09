55c0865c1478dfab204cc6de586b6e9e1e407498
agc en Supernova <agalveca@gmail.com>
2012-12-09 22:45:34 +0100

    SN terminada la aplicaion Rails 4 in action

---
+++ things_i_bought/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ThingsIBought::Application.config.secret_token = 'fc0a954d87d1b638f717d818eb05e9d580ee00a1730600b72e08146a0f2785bcb5e61319e8d9c7bafa44f848390ca354251d6a6e3ddef7b80487272f635fb1f9'
