f8d281019fcb793e1f0035da9249f2efda7c218c
agc en Supernova <agalveca@gmail.com>
2012-12-18 22:57:07 +0100

    SN terminado el capítulo 3 del libro de Oreilly

---
+++ librooreillyr3/hello01/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Hello01::Application.config.secret_token = 'b0a83b28d8a0725a48a6fa28236e3af0173ff47ffd3dbd38a015412cd5e1fe799b0dc2e671c54735c059b8cf2c1e2a436143f756ac1de74395b8dd7e40385f48'
