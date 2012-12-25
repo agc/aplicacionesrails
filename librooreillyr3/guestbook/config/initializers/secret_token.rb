f3bae792bb3052b5ed06c6192cbb6b05d8ed030f
agc en Supernova <agalveca@gmail.com>
2012-12-25 11:33:26 +0100

    SN cambios en librooreillyr3

---
+++ librooreillyr3/guestbook/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Guestbook::Application.config.secret_token = '171eb9652998662ab1bc188782c580689cbd79099073c387560c195d9f44d1b1171e4fb2a4550ba0a895bc8009c7bb92563c16da3cb753371349d7f421487dac'
