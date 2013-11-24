4edc7162505ac3d054e7c5d9382a63ba770447d2
agc en Nova <agc@pnte.cfnavarra.es>
2013-11-24 11:58:18 +0100

    N cambios en things_i_bought

--- things_i_bought/app/controllers/purchases_controller.rb
+++ things_i_bought/app/controllers/purchases_controller.rb
@@ -1,69 +1,51 @@
 class PurchasesController < ApplicationController
+  before_action :set_purchase, only: [:show, :edit, :update, :destroy]
+
   # GET /purchases
   # GET /purchases.json
   def index
     @purchases = Purchase.all
-
-    respond_to do |format|
-      format.html # index.html.erb
-      format.json { render json: @purchases }
-    end
   end
 
   # GET /purchases/1
   # GET /purchases/1.json
   def show
-    @purchase = Purchase.find(params[:id])
-
-    respond_to do |format|
-      format.html # show.html.erb
-      format.json { render json: @purchase }
-    end
   end
 
   # GET /purchases/new
-  # GET /purchases/new.json
   def new
     @purchase = Purchase.new
-
-    respond_to do |format|
-      format.html # new.html.erb
-      format.json { render json: @purchase }
-    end
   end
 
   # GET /purchases/1/edit
   def edit
-    @purchase = Purchase.find(params[:id])
   end
 
   # POST /purchases
   # POST /purchases.json
   def create
-    @purchase = Purchase.new(params[:purchase])
+    @purchase = Purchase.new(purchase_params)
 
     respond_to do |format|
       if @purchase.save
         format.html { redirect_to @purchase, notice: 'Purchase was successfully created.' }
-        format.json { render json: @purchase, status: :created, location: @purchase }
+        format.json { render action: 'show', status: :created, location: @purchase }
       else
-        format.html { render action: "new" }
+        format.html { render action: 'new' }
         format.json { render json: @purchase.errors, status: :unprocessable_entity }
       end
     end
   end
 
-  # PUT /purchases/1
-  # PUT /purchases/1.json
+  # PATCH/PUT /purchases/1
+  # PATCH/PUT /purchases/1.json
   def update
-    @purchase = Purchase.find(params[:id])
-
     respond_to do |format|
-      if @purchase.update_attributes(params[:purchase])
+      if @purchase.update(purchase_params)
         format.html { redirect_to @purchase, notice: 'Purchase was successfully updated.' }
         format.json { head :no_content }
       else
-        format.html { render action: "edit" }
+        format.html { render action: 'edit' }
         format.json { render json: @purchase.errors, status: :unprocessable_entity }
       end
     end
@@ -72,12 +54,21 @@ class PurchasesController < ApplicationController
   # DELETE /purchases/1
   # DELETE /purchases/1.json
   def destroy
-    @purchase = Purchase.find(params[:id])
     @purchase.destroy
-
     respond_to do |format|
       format.html { redirect_to purchases_url }
       format.json { head :no_content }
     end
   end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_purchase
+      @purchase = Purchase.find(params[:id])
+    end
+
+    # Never trust parameters from the scary internet, only allow the white list through.
+    def purchase_params
+      params.require(:purchase).permit(:name, :cost)
+    end
 end
--- things_i_bought/config/initializers/secret_token.rb
+++ things_i_bought/config/initializers/secret_token.rb
@@ -1,7 +1,12 @@
 # Be sure to restart your server when you modify this file.
 
-# Your secret key for verifying the integrity of signed cookies.
+# Your secret key is used for verifying the integrity of signed cookies.
 # If you change this key, all old signed cookies will become invalid!
+
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-ThingsIBought::Application.config.secret_token = 'fc0a954d87d1b638f717d818eb05e9d580ee00a1730600b72e08146a0f2785bcb5e61319e8d9c7bafa44f848390ca354251d6a6e3ddef7b80487272f635fb1f9'
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ThingsIBought::Application.config.secret_key_base = '1a5531d367f14f19763c622adcc9e12d30516fbd19238ea4a9c9cb6f52f1e58bed09f81bd107fe557959ccabe0462b5bc9c70848e28af41d9240876db318777e'
